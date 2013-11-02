1bd5ac218a7418b10a2b78362cef8a990fd1e825
burak firik <burakfirik@gmail.com>
2013-11-02 00:24:32 -0400

    weather API key is added

---
+++ WeatherAPIKey.h
@@ -0,0 +1,14 @@
+//
+//  WeatherAPIKey.h
+//  
+//
+//  Created by burak firik on 11/2/13.
+//
+//
+
+#ifndef _WeatherAPIKey_h
+#define _WeatherAPIKey_h
+
+static NSString *const kWeatherUndergroundAPIKey=@"49752d45514b7dd6"
+
+#endif
